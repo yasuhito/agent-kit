5c5fbd208f2db5e3cf1d9015021ea6084a7c54a6
Yasuhito Takamiya <yasuhito@hey.com>
2026-01-25 14:34:00 +0900

    test(web): run system tests and refresh filters

--- apps/web/config/boot.rb
+++ apps/web/config/boot.rb
@@ -1,4 +1,7 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+if ARGV.first&.start_with?("test")
+  ENV["DEFAULT_TEST_EXCLUDE"] ||= "test/{dummy,fixtures}/**/*_test.rb"
+end
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
